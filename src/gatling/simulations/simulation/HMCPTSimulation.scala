a1eeac5a0cfe21ffb19dd7848d931fd5705e71d9
Deepak Garcha <deepak.garcha@hmcts.net>
2022-09-28 17:26:30 +0100

    SoakTest

--- src/gatling/simulations/simulation/HMCPTSimulation.scala
+++ src/gatling/simulations/simulation/HMCPTSimulation.scala
@@ -122,7 +122,7 @@ import utils.{Environment, IDAMHelper, S2SHelper}
 
     //CreateAppealCase
     val CreateAppeal= scenario("CreateAppeal")
-     .repeat(10000){
+     .repeat(1){
      feed(NINumberFeeder)
      .exec(
          CreateAppealCase.CreateAppealCase,
@@ -142,11 +142,11 @@ import utils.{Environment, IDAMHelper, S2SHelper}
   //  .maxDuration(3600)
 
     //Request Hearing Smoke Tests
-  setUp(RH.inject(rampUsers(1700).during(3400)),  //1700 3400
-  (RUDH.inject(rampUsers(250).during(3200))), //250 3200
+  setUp(RH.inject(rampUsers(4400).during(14200)),  //1700 3400
+  (RUDH.inject(rampUsers(660).during(14200))), //250 3200
   (CreateAppeal.inject(rampUsers(1).during(1)))) //1
      .protocols(httpProtocol)
-     .maxDuration(3800)
+     .maxDuration(14600)
 
 //Soak test
 // RH 4400 / 14200
