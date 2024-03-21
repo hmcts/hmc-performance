7bd03687e66bc27f3a6d63170555d0db2f7c2549
Deepak Garcha <deepak.garcha@hmcts.net>
2024-03-21 16:37:42 +0000

    IAC HMC Run6 21032024

--- src/gatling/simulations/simulation/HMCPTSimulation.scala
+++ src/gatling/simulations/simulation/HMCPTSimulation.scala
@@ -170,24 +170,24 @@ import utils.{Environment, IDAMHelper, S2SHelper}
 
 
 //    CIVIL HMC Request Hearing Peak/Stress Test
-  setUp(
+  /*setUp(
     (RH.inject(rampUsers(1700).during(3300))),  //1700 3300
     (RUDH.inject(rampUsers(250).during(3200))), //250 3200
    (CreateAppeal.inject(rampUsers(1).during(100))))//1
      .protocols(httpProtocol)
     .maxDuration(4000)
 
-
+*/
     //Request Hearing Smoke Tests
-  /*setUp(RH.inject(rampUsers(1700).during(3400)),  //1700 3400
+  setUp(RH.inject(rampUsers(1700).during(3400)),  //1700 3400
     (RUDH.inject(rampUsers(250).during(3200))), //250 3200
   (CreateAppeal.inject(rampUsers(1).during(1)))) //1
      .protocols(httpProtocol)
      .maxDuration(4000)
-*/
+
 
 /*
-     setUp(RHR.inject(rampUsers(1864).during(2400)))
+     setUp(RHR.inject(rampUsers(1577).during(2400)))
          .protocols(httpProtocol)
       .maxDuration(30000)
 */
